c6d759571b0df09393b03515b4452eaae7538f9a
Agustín Ventura <agustinventura@gmail.com>
2017-05-07 19:17:42 +0200

    initial commit

---
+++ server/beer/src/main/java/com/digitalsingular/beer/configuration/BeerCommandLineRunner.java
@@ -0,0 +1,28 @@
+package com.digitalsingular.beer.configuration;
+
+import com.digitalsingular.beer.model.Beer;
+import com.digitalsingular.beer.repository.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
+
+@Component
+public class BeerCommandLineRunner implements CommandLineRunner {
+
+    private final BeerRepository repository;
+
+    public BeerCommandLineRunner(BeerRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        // Top beers from https://www.beeradvocate.com/lists/top/
+        Stream.of("Kentucky Brunch Brand Stout", "Good Morning", "Very Hazy", "King Julius",
+                "Budweiser", "Coors Light", "PBR").forEach(name ->
+                repository.save(new Beer(name))
+        );
+        repository.findAll().forEach(System.out::println);
+    }
+}
---
+++ server/beer/src/main/java/com/digitalsingular/beer/repository/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.digitalsingular.beer.repository;
+
+import com.digitalsingular.beer.model.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+}
